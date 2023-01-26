54e089e28f2f8d9be0a1f6e25330af2770171109
surim <122578934+surimjeon@users.noreply.github.com>
2023-01-26 14:54:53 +0900

    [Silver IV] Title: 설탕 배달, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,22 +1,15 @@
 kilo=int(input())
+count=0
 
 if kilo%5==0:
-    print(kilo//5)
-#생각 못했던 것: 5의 몫을 하나씩 줄어가기 보다는, 3씩 빼면서 5의 조합이 가능한지 푸는 방법
-else: 
-    p=0
-    while kilo>0: 
-        kilo-=3
-        p+=1
-        if kilo%5==0:#3kg과 5kg을 조합해서 담을 수 있을 때 
-            p+=kilo//5
-            print(p)
+    count=kilo//5
+else : 
+    for i in range(kilo//5,-1,-1):
+        mok=kilo-i*5
+        if mok%3==0:  #2번째 케이스, 3kg과 5kg을 조합해서 담을 수 있을 때. 3으로 나눠 떨어짐
+            count=i+mok//3
             break
-           
-        elif kilo==0:
-            print(p) #3으로 나눠 떨어질 때
-            break
+        else : 
+            count=-1
 
-        elif kilo==1 or kilo==2  : #5도 안나눠지고 3도 안나눠지면->최대 뺀 값이 1또는 2이면 
-            print(-1) 
-            break
+print(count)
