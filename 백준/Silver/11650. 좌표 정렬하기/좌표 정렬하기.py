922fd29f26260ca612bce1cf628fae3a179c9a1f
surim <122578934+surimjeon@users.noreply.github.com>
2023-01-30 12:09:09 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 408 ms, Memory: 45976 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -1,5 +1,5 @@
 import sys
-sys.stdin.readline
+input=sys.stdin.readline
 
 number=int(input())
 lst=[]
@@ -8,7 +8,7 @@ for i in range(number):
     x,y=map(int, input().split())
     lst.append([x,y])
 
-lst.sort(key=lambda x:(x[0],x[1]))
+lst.sort()
 
 for i in range(number):
     print(lst[i][0],lst[i][1])
