f18bc9114c4d8611e406800fcb0cf0a7c6c00f11
surim <122578934+surimjeon@users.noreply.github.com>
2023-02-01 11:22:03 +0900

    [Bronze I] Title: 직사각형 네개의 합집합의 면적 구하기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2669. 직사각형 네개의 합집합의 면적 구하기/직사각형 네개의 합집합의 면적 구하기.py
+++ 백준/Bronze/2669. 직사각형 네개의 합집합의 면적 구하기/직사각형 네개의 합집합의 면적 구하기.py
@@ -1,4 +1,3 @@
-#매트리스 100*100만들기
 
 blank=[[0]*101 for i in range(101)]
 
@@ -10,8 +9,8 @@ for i in range(4):
         for y in range(y1,y2):
             blank[x][y]=1
 
-for x in range(1,101):
-    for y in range(1,101):
+for x in range(100):
+    for y in range(100):
         if blank[x][y]==1:
             count+=1
 
