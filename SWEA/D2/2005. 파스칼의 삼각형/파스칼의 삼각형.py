b675b3f2869f9d91e981770faf5df80ae435d252
surim <122578934+surimjeon@users.noreply.github.com>
2023-02-14 11:54:24 +0900

    [D2] Title: 파스칼의 삼각형, Time: 92 ms, Memory: 45,300 KB -BaekjoonHub

--- SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.py
+++ SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.py
@@ -1,24 +1,17 @@
-T= int(input())
+TC=int(input())
 
-for tc in range(1, T+1):
+for tc in range(1,TC+1):
     n=int(input())
-
-    tri=[1]
+    mapp=[[0]*r for r in range(1,n+1)]
+    
     print(f'#{tc}')
-    print(*tri)
-    for i in range(n-1):
-        #현재줄의 맨처음과 끝에 있는 1은 더할게 없으니 0으로 채워준다
-        stack=[0]+tri+[0]
-        tri=[] #해당 줄에서 사용할 숫자를 저장할 리스트
-        #pop()을 하면 최근에 사용할 숫자를 저장할 리스트
-
-        num=stack.pop() #자신의 오르쪽 위 숫자 가져오기
-
-        while stack:
-            num2=stack.pop() #왼쪽 위 숫자 가져오기
-            tri.append(num+num2)
-
-            num = num2 #자신의 오른쪽 숫자를 왼쪽 숫자로 바꿔줌
-        print(*tri)
-
-            
+    for i in range(n):
+        for j in range(i+1): #이전행에서 사용했던 결과를 이용해서 현재행을 구하기
+            if j==0:
+                mapp[i][j]=1
+            elif i==j:
+                mapp[i][j]=1
+            else:
+                mapp[i][j]=mapp[i-1][j-1]+mapp[i-1][j]
+    
+        print(*mapp[i])
