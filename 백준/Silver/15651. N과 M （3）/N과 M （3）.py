1a65b42d4bd3e00058943098faaf6e6f1935817b
surim <122578934+surimjeon@users.noreply.github.com>
2023-02-16 10:03:38 +0900

    [Silver III] Title: N과 M (3), Time: 4368 ms, Memory: 155984 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,19 @@
+def johap(arr, n): # 배열과, n:개수
+    result = []
+    if n==0:
+        return [[]]
+    for i in range(len(arr)):
+        elem=arr[i]
+        for rest in johap(arr,n-1):
+            result.append([elem]+rest)
+    return result
+
+n,m=map(int,input().split())
+arr=[i for i in range(1,n+1)]
+
+
+for i in johap(arr,m):
+    ans=i
+    print(*i)
+
+
