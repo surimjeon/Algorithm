5c4b5bd3266068dec521514ab107603ec0979d21
surim <122578934+surimjeon@users.noreply.github.com>
2023-02-17 17:10:57 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 1,156 ms, Memory: 248,012 KB -BaekjoonHub

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
@@ -3,17 +3,15 @@ TC = int(input())
 for tc in range(1,TC+1):
     n = int(input())
     stack = list(map(int, input().split()))
-    result = 0
 
-    #스택에 넣어져있는 최대값을 매기는 변수
+    result=0
+
     mx=stack.pop()
     while stack:
-        #스택 요소를 하나씩 빼내면서 mx최대값을 갱신
         price=stack.pop()
         if mx<price:
             mx=price
         else:
-            result+= mx-price
-    print(f'#{tc} {result}')
+            result+=(mx-price) #최대값에서 요소들을 뺀값이 결과값
 
-        #최대값이 갱신되지 않는 값이라면 사야하는 금액
+    print(f'#{tc} {result}')
