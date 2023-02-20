dad710479689a4a9bfaa0a332db2844f3300a237
surim <122578934+surimjeon@users.noreply.github.com>
2023-02-20 19:34:58 +0900

    [Silver IV] Title: 큐 2, Time: 1824 ms, Memory: 175104 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,32 @@
+import sys
+from collections import deque
+
+n=int(sys.stdin.readline())
+queue=deque()
+for i in range(n):
+    state=list(sys.stdin.readline().split())
+    if state[0]=='push':
+        queue.append(state[1])
+    elif state[0]=='pop':
+        if len(queue)==0:
+            print(-1)
+        else:
+            a=queue.popleft()
+            print(a)
+    elif state[0]=='size':
+        print(len(queue))
+    elif state[0]=='empty':
+        if queue:
+            print(0)
+        else:
+            print(1)
+    elif state[0]=='front':
+        if queue:
+            print(queue[0])
+        else:
+            print(-1)
+    elif state[0]=='back':
+        if queue:
+            print(queue[-1])
+        else:print(-1)
+    
