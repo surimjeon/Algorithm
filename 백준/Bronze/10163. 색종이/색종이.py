fe4a3d1c0065ef346e980f7892d39dfc5ecb8639
surim <122578934+surimjeon@users.noreply.github.com>
2023-02-28 22:47:35 +0900

    [Bronze I] Title: 색종이, Time: 152 ms, Memory: 38304 KB -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,9 +1,11 @@
-n=int(input())
+import sys
+
+n=int(sys.stdin.readline())
 
 mapp=[[0]*1001 for _ in range(1001)]
 
 for k in range(1,n+1):
-    x,y,w,h=list(map(int,input().split()))
+    x,y,w,h=list(map(int,sys.stdin.readline().split()))
     for i in range(x,x+w): #123
         for j in range(y,y+h): #4,5
             mapp[i][j]=k
@@ -13,3 +15,5 @@ for i in range(1,n+1):
     for m in mapp: #파이썬 한줄에서 해당 숫자가 얼마만큼있는지 세기
         cnt+=m.count(i)
     print(cnt)
+
+
