c2eecb93dfbb80adb460afa0574be28fa8f79029
surim <122578934+surimjeon@users.noreply.github.com>
2023-03-04 23:31:05 +0900

    [Bronze II] Title: 숫자, Time: 96 ms, Memory: 36676 KB -BaekjoonHub

--- 백준/Bronze/10093. 숫자/숫자.py
+++ 백준/Bronze/10093. 숫자/숫자.py
@@ -4,12 +4,8 @@ mn=min(a,b)
 mx=max(a,b)
 cnt=0
 lst=[]
-if mn==mx:
-    print(cnt)
-    print()
-else:
-    for i in range(mn+1,mx):
-        cnt+=1
-        lst.append(i)
-    print(cnt)
-    print(*lst)
+for i in range(mn+1,mx):
+    cnt+=1
+    lst.append(i)
+print(cnt)
+print(*lst)
