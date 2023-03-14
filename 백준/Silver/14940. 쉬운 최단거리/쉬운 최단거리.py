84033d879b8207f600476697755de308f9f75db8
surim <122578934+surimjeon@users.noreply.github.com>
2023-03-15 01:38:28 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 520 ms, Memory: 41912 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -1,22 +1,24 @@
 #모든 지점에 대해 목표지점까지의 거리 구하기
 from collections import deque
+
+#bfs구현코드
 def bfs(start_x, start_y):
     quque.append((start_x, start_y))
     visited[start_x][start_y]=True
-    ans[start_x][start_y]=0
+    ans[start_x][start_y]=0 #자기자신은 
+
     while quque:
         x,y=quque.popleft()
-        
         for k in range(4):
             nx=x+dx[k]
             ny=y+dy[k]
             if 0<=nx and nx<n and 0<=ny and ny<m and visited[nx][ny]==False:
-                if mapp[nx][ny]==1:
-                    ans[nx][ny]=ans[x][y]+1            
-                    quque.append((nx,ny))
-                    visited[nx][ny]=True
-                elif mapp[nx][ny]==0:
-                        ans[nx][ny]=0
+                if mapp[nx][ny]==1:     #길(1)이 있으면 
+                    ans[nx][ny]=ans[x][y]+1  #cnt누적  
+                    quque.append((nx,ny))   #queue에 추가
+                    visited[nx][ny]=True   #방문체크
+                # elif mapp[nx][ny]==0:       #길이 없다면 
+                #         ans[nx][ny]=0   #답을 0으로 체크
             
        
 n,m=map(int,input().split())
@@ -27,7 +29,7 @@ dx=[1,0,-1,0]
 dy=[0,1,0,-1]
 
 quque=deque()
-ans=[[-1]*m for _ in range(n)] #안간곳은 -1 
+ans=[[-1]*m for _ in range(n)] #안간곳은 -1(도달할 수 없는 위치이면 -1)
 visited = [[False]* m for _ in range(n)]
 
 for i in range(n):
@@ -36,8 +38,10 @@ for i in range(n):
         if mapp[i][j]==2:
             start_x,start_y=i,j
 
-bfs(start_x,start_y)
+bfs(start_x,start_y) #start부터 모든 점에 대해 돌꺼니까 여기서 for문돌릴 필요가 없다
 
+#ex)mapp에서 0으로 둘러싸여있는 아이들은 -1이니
+#mapp=0인 곳에 -1이 나오는 경우 0으로 바꿔주는 처리
 for i in range(n):
     for j in range(m):
         if ans[i][j]==-1 and mapp[i][j]==0:
