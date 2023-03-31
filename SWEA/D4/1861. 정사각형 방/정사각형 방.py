7f5c25f3a25ddae41861f1aac3cc7c401c90409a
surim <122578934+surimjeon@users.noreply.github.com>
2023-03-31 14:25:00 +0900

    [D4] Title: 정사각형 방, Time: 2,369 ms, Memory: 187,128 KB -BaekjoonHub

--- SWEA/D4/1861. 정사각형 방/정사각형 방.py
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.py
@@ -1,28 +1,43 @@
+from collections import deque
 
-def check(x,y):
-    dx=[1,0,-1,0]
-    dy=[0,-1,0,1]
-    global cnt
-    for k in range(4):
-        nx=x+dx[k]
-        ny=y+dy[k]
-        if 0<=nx<n and 0<=ny<n and mapp[nx][ny]-mapp[x][y]==1:
-            cnt+=1
-            check(nx,ny)
+def dfs(x,y,cnt):
+    global max_len
+    deq=deque()
+    dx=[0,1,0,-1]
+    dy=[1,0,-1,0]
+    deq.append((x, y, cnt))
+    visitied[x][y] = 1
+    while deq:
+        x,y,cnt=deq.popleft()
+        for k in range(4):
+            nx=x+dx[k]
+            ny=y+dy[k]
+            if 0<=nx<N and 0<=ny<N:
+                if matrix[nx][ny]==matrix[x][y]+1:
+                    deq.append((nx,ny,cnt+1))
+                    visitied[x][y]=1
+    if cnt>max_len:
+        max_len=cnt
 
-T=int(input())
 
-for tc in range(1,T+1):
-    n=int(input())
-    mapp=[list(map(int,input().split())) for _ in range(n)]
- 
-    lst=[0]*(n**2+1)
-    for i in range(n):
-        for j in range(n):
-            cnt=1
-            current=mapp[i][j]
-            check(i,j)
-            lst[current]=cnt #처음 출발 방번호를 index로 지정해놓고, count를 값으로 지정해줘서 찾았다
-    room=max(lst)
-    room_n=lst.index(room)
-    print(f'#{tc} {room_n} {room}')
+
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    matrix = [list(map(int, input().split())) for _ in range(N)]
+    ans = 0
+    visitied = [[0 for _ in range(N)] for _ in range(N)]
+    for i in range(N):
+        for j in range(N):
+            if not visitied[i][j]:
+                max_len = 0
+                dfs(i, j, 1)
+                if max_len > ans:
+                    ans = max_len
+                    n = matrix[i][j]
+                elif max_len == ans and matrix[i][j] < n:
+                    n = matrix[i][j]
+
+    
+
+    print(f'#{tc} {n} {ans}')
