4aa911a0933d87e1b707c21cccdebf923942e1ad
surim <122578934+surimjeon@users.noreply.github.com>
2023-04-10 10:48:43 +0900

    [Gold V] Title: 치킨 배달, Time: 340 ms, Memory: 34192 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,27 +1,52 @@
+'''
+치킨을 dfs, 백트래킹으로 푸는 경우
+'''
 
-from itertools import combinations
+from collections import deque
+N,M = map(int, input().split())                                # 도시 크기, 최대 치킨집 수
+mapp = [list(map(int, input().split())) for _ in range(N)]
 
-n,m=map(int,input().split()) #m:치킨집 개수
-mapp=[list(map(int,input().split())) for _ in range(n)]
-ans=999999
-house=[]
-chicken=[]
+def dfs(n,i): #n: 고른 치킨집의 수, i: 치킨집 번호(한번씩 돌아가면서 )
+    global result
+    val = 0
+    # 모든 치킨집을 골랐다면 치킨거리 계산
+    if n == M:
+        for h in house:
+            hr, hc = h[0], h[1]
+            dist = 2*N
 
-for i in range(n):
-    for j in range(n):
-        if mapp[i][j] == 1:
+            for c in select:
+                cr, cc = c[0], c[1]
+                tmp = abs(hr-cr) + abs(hc-cc)
+                if tmp < dist:
+                    dist = tmp  #각 집의 치킨거리 계산
+            val += dist #도시전체의 치킨거리를 추가하면서
+
+        if val < result: #최소 도시전체의 치킨거리 계산하고 return
+            result = min(val, result)
+            return 
+    
+    for idx in range(i,k): #치킨집 번호부터 총 치킨집 개수까지 넣어보면서 dfs추가해보기
+        select.append(chicken[idx]) #치킨집을 하나씩 넣어보고
+        dfs(n+1, idx+1) 			#치킨집 개수와 치킨집번호를 +1씩 증가하기
+        select.pop()				#다시 원상복귀
+
+
+house= deque([])
+chicken=deque([])
+select = deque([])
+
+
+for i in range(N):
+    for j in range(N):
+        if mapp[i][j]==1:
             house.append((i,j))
-        elif mapp[i][j] == 2:
+        elif mapp[i][j]==2:
             chicken.append((i,j))
 
-#1개부터 m개까지 조합을 구하기
-
-for c in combinations(chicken,m):
-    total_d=0
-    for r1, c1 in house:
-        d=99999
-        for r2, c2 in c : #치킨집 선택 조합의 튜플 묶음
-            d=min(d,abs(r1-r2)+abs(c1-c2)) #각 치킨집 간의 거리 중에 가장 최소값을 찾기
-        total_d+=d                   #도시의 모든 치킨거리의 합
-    ans=min(total_d, ans) # 모든조합에서 가장 최소인값 = 정답
-print(ans)
+k =len(chicken) #치킨집의 총 개수
+result = 999999
+#조합 시작
+for idx in range(k): #chicken 큐에 들어있는, 치킨집 번호
+    dfs(0,idx)
+print(result)
