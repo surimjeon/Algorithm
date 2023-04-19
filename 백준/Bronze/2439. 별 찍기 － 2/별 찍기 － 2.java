7523c4b12db987fe1ac199602fb1fa4403cabf3f
surim <122578934+surimjeon@users.noreply.github.com>
2023-04-19 16:58:20 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 360 ms, Memory: 18248 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for (int i=1; i<=n; i++) {
+			for (int j=0; j<=n-i-1; j++) {
+				System.out.print(" ");
+			}
+			for (int k=1;k<=i; k++) {
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
+	}
+
+}
