a3b6087916e7d9c62de760d9d86f7e59db4d6c19
surim <122578934+surimjeon@users.noreply.github.com>
2023-04-22 21:53:24 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 144 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,33 @@
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] ab=br.readLine().split(" ");
+		StringBuilder sb = new StringBuilder();
+		int n=Integer.parseInt(ab[0]);
+		int m=Integer.parseInt(ab[1]);
+		int[][] arr1=new int[n][m];
+		
+	for (int k=0; k<2; k++) {
+		for(int i=0; i<n;i++) {
+			String[] temp=br.readLine().split(" ");
+			for (int j=0;j<m;j++) {
+				arr1[i][j]+=Integer.parseInt(temp[j]); //한줄
+			}
+		}
+		
+		}
+		for (int i=0; i<n; i++) {
+			for(int j=0; j<m; j++) {
+				sb.append(arr1[i][j]).append(" ");
+			}
+			System.out.println(sb);
+			sb.setLength(0);
+		}
+		
+		}
+			}
