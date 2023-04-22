6da9b7584ee41e5ebd586e5545109f2db9559a06
surim <122578934+surimjeon@users.noreply.github.com>
2023-04-22 09:48:47 +0900

    [Bronze V] Title: 아스키 코드, Time: 76 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String S =br.readLine();
+		
+		System.out.println((int)S.charAt(0));
+		}
+	}
