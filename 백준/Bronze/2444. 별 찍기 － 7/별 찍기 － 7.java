9578f683a7838d47d7da1a637acbe4172fb11b8b
surim <122578934+surimjeon@users.noreply.github.com>
2023-04-25 00:25:20 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 88 ms, Memory: 11716 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -5,24 +5,24 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
 		for (int i=1; i<=n;i++) {
 			for (int j=1; j<=n-i; j++) { //1~4
-			System.out.print(' ');}
+				sb.append(" ");}
 			for (int j=1; j<=2*i-1; j++){
-				System.out.print("*");
+				sb.append("*");;
 			}
-			System.out.println();
+			sb.append("\n");
 		}
 		
 		for (int i=n-1; i>=1;i--) {
 			for (int j=1; j<=n-i; j++) { //1~4
-			System.out.print(' ');}
+				sb.append(" ");}
 			for (int j=1; j<=2*i-1; j++){
-				System.out.print("*");
+				sb.append("*");;
 			}
-			System.out.println();
+			sb.append("\n");
 		}
-		
+		System.out.println(sb);
 	}
 }
-		
