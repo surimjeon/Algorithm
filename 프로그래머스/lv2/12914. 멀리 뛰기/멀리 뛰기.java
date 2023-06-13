e8c336091a4095901622136235378e18147e7f5f
surim <122578934+surimjeon@users.noreply.github.com>
2023-06-14 00:07:41 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.12 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long solution(int n) {
+        long[] memo = new long[2001]; //최대값
+        memo[1]=1;
+        memo[2]=2;
+        
+        for (int i=3; i<n+1; i++) {
+            memo[i]=(memo[i-1]+memo[i-2])%1234567; //DP
+        }
+        return memo[n];
+    }
+}
