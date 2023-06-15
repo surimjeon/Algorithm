0ff4b12002b32d8ed81f494547a21fcb63795d25
surim <122578934+surimjeon@users.noreply.github.com>
2023-06-15 19:22:35 +0900

    [level 2] Title: 피로도, Time: 0.69 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.java
+++ 프로그래머스/lv2/87946. 피로도/피로도.java
@@ -8,27 +8,21 @@ class Solution {
         Arrays.sort(dungeons, (o1,o2)-> { return o2[0]-o1[0];});
         visited = new boolean[dungeons.length];
         
-        dfs(k, dungeons);
+        dfs(k, dungeons,0);
         return mx;
     }
-    public static void dfs(int k, int[][] dungeons) {
-        //현재까지 포함하면서 mx값 갱신 return
-        int ans = 0;
-        for (int i=0; i<dungeons.length; i++) {
-            if (visited[i]) ans++;
-        }
-        mx = Math.max(ans, mx);
-        
+    public static void dfs(int k, int[][] dungeons, int cnt) {
         //전체 던전을 돌면서 하나씩 넣어보고 빼보기
         for (int i=0; i<dungeons.length; i++) {
              //던전의 0번쨰가 현재 k보다 작거나 같고, 방문하지 않았다면 
             if(dungeons[i][0]<=k && visited[i]==false) {
                 k-=dungeons[i][1];
                 visited[i]=true; // 그렇다면 던전 1번째를 현재 k에서 빼고 방문처리
-                dfs(k,dungeons);
+                dfs(k,dungeons, cnt+1);
                 k+=dungeons[i][1];
                 visited[i]=false; //k도 넣기, 다시 방문처리 false해주고
             } 
+            mx = Math.max(cnt, mx);
         }
     }
 }
