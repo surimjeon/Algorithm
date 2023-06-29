3bdbaff081d0e29fb1d9f4a4a46fd82d433c7384
surim <122578934+surimjeon@users.noreply.github.com>
2023-06-29 17:13:41 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.25 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,23 +1,23 @@
+import java.util.*;
 class Solution {
     public String solution(String number, int k) {
         String answer = "";
-        // 제일 앞수가 크면 가장 큰거...?  그러고 나서 k를 못채우면, 다음 자리수를 제거
-        int idx =0; //시작위치는 최대값 위치부터
-        StringBuilder st = new StringBuilder();
+        //정답배열
+        char [] ans = new char[number.length()-k];
+        Stack<Character> st = new Stack<>();
         
-        for (int i=0; i<number.length()-k; i++) { //6자리 만들기
-            int mx = 0;
-            for (int j=idx; j<=i+k; j++) { //범위의 시작~k자리만큼 끊으면서 큰수비교
-                int temp = number.charAt(j)-'0';
-                if (mx<temp) {
-                    mx = temp;
-                    idx = j+1;
-                }
+        for (int i=0; i<number.length(); i++) {
+            char c = number.charAt(i);
+            while (!st.isEmpty() && st.peek()<c && k-->0) {
+                st.pop();
             }
-            st.append(mx);
+            st.push(c);
         }
-        
-            
-        return st.toString();
+        // System.out.println(st.size());
+        for (int i=0; i<ans.length; i++) {
+            ans[i]=st.get(i);
+        }
+    
+        return new String(ans);
     }
 }
