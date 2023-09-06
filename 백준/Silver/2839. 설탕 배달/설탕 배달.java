2f1a2b411e987ced63ede26715739fd0e7ba71e5
surim <122578934+surimjeon@users.noreply.github.com>
2023-09-06 23:09:24 +0900

    [Silver IV] Title: 설탕 배달, Time: 76 ms, Memory: 11476 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int answer = 0;
+        int N = Integer.parseInt(br.readLine());
+        //5를 계속 되는데까지 하고 안되면 3으로 나눠떨어지는지 보고
+        //만약 안나눠떨어지면 -1 아님 그대로
+
+        if (N == 4 || N == 7) {
+            System.out.println(-1);
+        }
+        else if (N % 5 == 0) {
+            System.out.println(N / 5);
+        }
+        else if (N % 5 == 1 || N % 5 == 3) {
+            System.out.println((N / 5) + 1);
+        }
+        else if (N % 5 == 2 || N % 5 == 4) {
+            System.out.println((N / 5) + 2);
+        }
+        }
+
+
+    }
