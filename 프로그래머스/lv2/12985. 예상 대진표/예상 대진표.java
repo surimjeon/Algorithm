c278a84e57dfbe88faaf4855b75e5ca6996ca008
surim <122578934+surimjeon@users.noreply.github.com>
2023-09-15 23:37:06 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.java
@@ -2,17 +2,17 @@ class Solution
 {
     public int solution(int n, int a, int b)
     {
-        int answer = 1;
-        //각각의 순서는 /2씩 줄어든다
-        while (true) {
-            a = a/2 + a%2;
-            b = b/2 + b%2;
-            
-            if (a==b) {
-                break;
-            }
-            answer++; 
+        int answer = 0;
+        //a는 b와 붙기 전까지 항상이기고, 몇번째 라운드에서 만나나요?
+        //12 /34 /56 / 78
+        // 1 / 4 / 5 / 7     /// 4->2->1 / 8->4->2 (옆수이면 count종료)
+        
+        while(a!=b) {
+            a=(a+1)/2;
+            b= (b+1)/2;
+            answer++;
         }
+        
         
         return answer;
     }
