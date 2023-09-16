2642b1ef109121c7edd6b8663269b0686c10f8e8
surim <122578934+surimjeon@users.noreply.github.com>
2023-09-16 15:40:21 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.05 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,23 +1,24 @@
-import java.util.*;
 class Solution {
     public String solution(String number, int k) {
         String answer = "";
-        //정답배열
-        char [] ans = new char[number.length()-k];
-        Stack<Character> st = new Stack<>();
+        StringBuilder sb = new StringBuilder();
         
-        for (int i=0; i<number.length(); i++) {
-            char c = number.charAt(i);
-            while (!st.isEmpty() && st.peek()<c && k-->0) {
-                st.pop();
+        int idx=0;
+        //전체 자리 수에서 - k인 만큼의 숫자를 반환할 필요가 있음
+        for (int i=0; i<number.length()-k; i++) { //0~6
+            int mx =0;
+            for(int j=idx; j<=i+k; j++) {
+                if (mx<number.charAt(j)-'0') {
+                    mx=number.charAt(j)-'0';
+                    idx=j+1;
+                }
+                
             }
-            st.push(c);
+            sb.append(mx);
         }
-        // System.out.println(st.size());
-        for (int i=0; i<ans.length; i++) {
-            ans[i]=st.get(i);
-        }
-    
-        return new String(ans);
+        
+        
+        
+        return sb.toString();
     }
 }
