da1d8af677f52d237fe454c5af76a1ce5ae93f72
surim <122578934+surimjeon@users.noreply.github.com>
2023-09-18 23:01:55 +0900

    [level 2] Title: 주식가격, Time: 36.43 ms, Memory: 67.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.java
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -2,14 +2,21 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
+        
+        Stack<Integer> st = new Stack<>();
+        
         for (int i=0; i<prices.length; i++) {
-            for (int j=i+1; j<prices.length; j++) {
-                answer[i]++;
-                if (prices[i]>prices[j]) {
-                    break;
-                }
+            while(!st.isEmpty() && prices[st.peek()]>prices[i]) {
+                answer[st.peek()] = i-st.peek();
+                st.pop();
             }
+            st.push(i);
         }
+        while(!st.isEmpty()) {
+            answer[st.peek()] = prices.length - st.peek()-1;
+            st.pop();
+        }
+        
         return answer;
     }
 }
